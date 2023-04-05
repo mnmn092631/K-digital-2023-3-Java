840030381c853aba24be3ee4606ecb14d0045efc
juhee kang <mnmn092631@gmail.com>
2023-04-05 16:57:41 +0900

    명령행 매개변수 이용 실습

--- lecture/src/chapter1/Test1.java
+++ lecture/src/chapter1/Test1.java
@@ -3,9 +3,7 @@ package chapter1;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Java");
-		System.out.println("안녕하세요");
+		System.out.println("Hello " + args[0]);
 	}
 
 }
