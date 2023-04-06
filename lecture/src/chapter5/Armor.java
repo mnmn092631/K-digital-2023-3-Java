de8490e70689f6314b18f98fb3f857faa31cceb2
juhee kang <mnmn092631@gmail.com>
2023-04-06 15:38:45 +0900

    클래스

---
+++ lecture/src/chapter5/Armor.java
@@ -0,0 +1,21 @@
+package chapter5;
+
+public class Armor {
+	
+	private String name;
+	private int height;
+	private int weight;
+	private String color;
+	private boolean isFly;
+	
+	public void setName() {
+		String value = "mark6";
+		name = value;
+	}
+	
+	public String getName() {
+		if(name == null) return "No Name";
+		return name;
+	}
+	
+}
---
+++ lecture/src/chapter5/Exam01.java
@@ -0,0 +1,21 @@
+package chapter5;
+
+public class Exam01 {
+
+//	private static int methodSum(int i, int j) {
+//		return i + j;
+//	}
+//	
+//	public static void main(String[] args) {
+//		int sum = methodSum(15, 20);
+//		System.out.print(sum);
+//	}
+	
+	public static void main(String[] args) {
+		Armor armor = new Armor();
+//		armor.setName();
+		String name = armor.getName();
+		System.out.print("Name : " +  name);
+	}
+
+}
