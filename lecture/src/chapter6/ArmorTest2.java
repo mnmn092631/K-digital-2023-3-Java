ec6cbea6f5b261ed271464b9380db4e51319b333
juhee kang <mnmn092631@gmail.com>
2023-04-07 16:38:00 +0900

    null

---
+++ lecture/src/chapter6/ArmorTest2.java
@@ -0,0 +1,13 @@
+package chapter6;
+
+public class ArmorTest2 {
+
+	public static void main(String[] args) {
+		Armor suit = new Armor();
+		suit = null;
+		suit.setName("mark6");
+		suit.setHeight(180);
+		System.out.println(suit.getName() + " : " + suit.getHeight());
+	}
+
+}
