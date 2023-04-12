9879a4d7505424dd0782137f132c84fb6f45285b
juhee kang <mnmn092631@gmail.com>
2023-04-12 17:24:56 +0900

    Messenger interface

---
+++ lecture/src/chapter7/abstraction/IPhoneMessenger.java
@@ -0,0 +1,19 @@
+package chapter7.abstraction;
+
+public class IPhoneMessenger implements Messenger {
+
+	@Override
+	public String getMessage() {
+		return "iPhone";
+	}
+
+	@Override
+	public void setMessage(String msg) {
+		System.out.println("iPhone에서 메시지를 설정합니다 : " + msg);
+	}
+	
+	public void clearMessage() {
+		System.out.println("좌우로 흔들어 문자열을 지웁니다.");
+	}
+
+}
