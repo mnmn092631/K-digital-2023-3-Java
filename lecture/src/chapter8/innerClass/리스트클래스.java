7763469746e30aec0406248e650d67e74363c7c7
juhee kang <mnmn092631@gmail.com>
2023-04-14 16:58:45 +0900

    내부클래스

---
+++ lecture/src/chapter8/innerClass/리스트클래스.java
@@ -0,0 +1,48 @@
+package chapter8.innerClass;
+
+class Node {
+	String data;
+	Node link;
+
+	public Node(String data) {
+		this.data = data;
+	}
+
+	public String toString() {
+		return "data : " + data;
+	}
+}
+
+class LinkedList {
+	Node head;
+
+	void add(String name) {
+		Node temp = new Node(name);
+		if (head == null)
+			head = temp;
+		else {
+			temp.link = head;
+			head = temp;
+		}
+	}
+
+	void show() {
+		Node p = head;
+		while (p != null) {
+			System.out.println(p.toString());
+			p = p.link;
+		}
+	}
+}
+
+public class 리스트클래스 {
+	public static void main(String[] args) {
+		LinkedList my = new LinkedList();
+		my.add("hong");
+		my.add("kim");
+		my.add("lee");
+		my.add("park");
+		my.add("kang");
+		my.show();
+	}
+}
