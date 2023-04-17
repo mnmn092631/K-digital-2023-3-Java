614415930d72aea4424455f2c1819f454c752cba
juhee kang <mnmn092631@gmail.com>
2023-04-17 10:04:26 +0900

    기본 API - Object 클래스

--- lecture/src/chapter9/Test01.java
+++ lecture/src/chapter9/Test01.java
@@ -4,10 +4,35 @@ public class Test01 {
 	public static void main(String[] args) {
 		Object obj1 = new Object();
 		Object obj2 = new Object();
-		
-		System.out.println("hashCode : " + obj1.hashCode());
-		System.out.println("toString : " + obj1.toString());
-		System.out.println("== : " + (obj1 == obj2));
-		System.out.println("equals : " + obj1.equals(obj2));
+		Object obj3 = new Object();
+		Class c = obj1.getClass();
+		MyObject obj4 = new MyObject(123);
+		MyObject obj5 = new MyObject(123);
+
+		System.out.println("obj1.hashCode : " + obj1.hashCode());
+		System.out.println("obj2.hashCode : " + obj2.hashCode());
+		System.out.println("obj3.hashCode : " + obj3.hashCode());
+
+		System.out.println(c.getName());
+
+		System.out.println(obj1.toString());
+		System.out.println(obj2.toString());
+		System.out.println(obj3.toString());
+
+		System.out.println(obj1);
+		System.out.println(obj2);
+		System.out.println(obj3);
+
+		if (obj4.equals(obj5)) {
+			System.out.println("동일 객체이다.");
+		} else {
+			System.out.println("다른 객체이다.");
+		}
+
+		if (obj4 == obj5) {
+			System.out.println("동일 객체이다.");
+		} else {
+			System.out.println("다른 객체이다.");
+		}
 	}
 }
