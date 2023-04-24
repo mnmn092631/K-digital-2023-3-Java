4e5e838008fa6ddb9c307d111327e5b6c1d63be2
juhee kang <mnmn092631@gmail.com>
2023-04-24 11:47:02 +0900

    타입 제한

---
+++ lecture/src/chapter13/BagTest_2.java
@@ -0,0 +1,54 @@
+package chapter13;
+
+class Bag2<T extends Solid> {
+	private T thing;
+
+	public Bag2(T thing) {
+		this.thing = thing;
+	}
+
+	public T getThing() {
+		return thing;
+	}
+
+	public void setThing(T thing) {
+		this.thing = thing;
+	}
+
+	void showType() {
+		System.out.println("T의 타입은 " + thing.getClass().getName());
+	}
+}
+
+class Solid {
+}
+
+class Liquid {
+}
+
+class Book2 extends Solid {
+}
+
+class PencilCase2 extends Solid {
+}
+
+class Notebook2 extends Solid {
+}
+
+class Water extends Liquid {
+}
+
+class Coffee extends Liquid {
+}
+
+public class BagTest_2 {
+
+	public static void main(String[] args) {
+		Bag2<Book2> bag = new Bag2<>(new Book2());
+		Bag2<PencilCase2> bag2 = new Bag2<>(new PencilCase2());
+		Bag2<Notebook2> bag3 = new Bag2<>(new Notebook2());
+
+//		Bag2<Water> bag4 = new Bag2<>(new Water()); 오류 발생
+	}
+
+}
